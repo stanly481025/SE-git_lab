a6d45bf8c3af323be904d3d842b95024339dad5b
stanly481025 <33078542+stanly481025@users.noreply.github.com>
2017-10-25 11:09:33 +0800

    ver 2
    我寫日文英文德文版我愛你

---
+++ Gittest.java
@@ -0,0 +1,9 @@
+public class Gittest
+{
+	public static void main(String[] argv)
+	{
+		System.out.println("私はあなたを愛している"); //日文
+		System.out.println("I love you."); //英文
+		System.out.println("Ich liebe dich"); //德文
+	}
+}
