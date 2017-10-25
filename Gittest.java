c109a19c97cbac9863ef1127d59a74c41c256809
cao5533 <33078539+cao5533@users.noreply.github.com>
2017-10-25 11:20:38 +0800

    Update by 00457031

--- Gittest.java
+++ Gittest.java
@@ -5,9 +5,14 @@ public class Gittest
 		System.out.println("私はあなたを愛している"); //日文
 		System.out.println("I love you."); //英文
 		System.out.println("Ich liebe dich"); //德文
-		System.out.println("Je t'aime"); //法文
-		System.out.println("나는 너를 사랑해."); //韓文
-		System.out.println("Ég elska þig"); //冰島文
-		System.out.println("Rakastan sinua"); //芬蘭文
+	}
+}
+public class hello
+{
+	public static void main(String[] argv)
+	{
+		System.out.println("Jeg elsker dig"); //丹麥文
+		System.out.println("Testimo."); //家泰蘭文
+		System.out.println("Ngiyakuthanda"); //祖魯文
 	}
 }
